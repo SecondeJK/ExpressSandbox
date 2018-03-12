210d7e8cbda10292f377642c084c1cf0810d6fcb
Jim Seconde <jim.seconde@googlemail.com>
2018-03-12 00:02:26 +0000

    added route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/testRoute', function(req, res, next) {
+    res.render('newtemplate', { newText: 'New routing text' });
+});
+
 module.exports = router;
